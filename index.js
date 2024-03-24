953694baba9fb5ef294056aeb18f30ad85236f79
dan20 <kiptoodaniel92@gmail.com>
2024-03-24 13:29:12 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name){
+    return cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name){
+    return cats.shift(name);
+}
+function appendCat(name){
+    var newCats=cats.slice();
+    newCats.push(name);
+    return newCats;
+}
+function prependCat(name){
+    const prepCat=cats.slice();
+    prepCat.unshift(name);
+    return prepCat;
+}
+function removeLastCat(){
+    const oneDown=cats.slice();
+    oneDown.pop(name);
+    return oneDown;
+}
+function removeFirstCat(){
+    const oneUp=cats.slice();
+    oneUp.shift(name);
+    return oneUp;
+}
